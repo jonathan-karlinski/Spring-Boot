e12ee9b0c48fcf1a8091db50b78d32b018d29038
jonathan-karlinski <jonathankarlinski57@gmail.com>
2020-04-20 11:54:57 -0300

    Exemplo Maven

---
+++ exemplomaven/src/main/java/karlinski/jonathan/exemplomaven/ExemplomavenApplication.java
@@ -0,0 +1,16 @@
+package karlinski.jonathan.exemplomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplomavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplomavenApplication.class, args);
+
+		System.out.println("Hello Word SpringBOOT com maven");
+
+	}
+
+}
---
+++ exemplomaven/src/test/java/karlinski/jonathan/exemplomaven/ExemplomavenApplicationTests.java
@@ -0,0 +1,13 @@
+package karlinski.jonathan.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplomavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
