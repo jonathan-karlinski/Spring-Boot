41290504a77caddc44e96224506ba446f566f467
jonathan-karlinski <jonathankarlinski57@gmail.com>
2020-04-20 11:57:31 -0300

    Exemplo Gradle Rest Repositories

---
+++ exemplogradleweb/src/main/java/karlinski/jonathan/exemplogradleweb/ExemplogradlewebApplication.java
@@ -0,0 +1,15 @@
+package karlinski.jonathan.exemplogradleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplogradlewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplogradlewebApplication.class, args);
+
+		System.out.println("Exemplo com server Tomcat");
+	}
+
+}
---
+++ exemplogradleweb/src/test/java/karlinski/jonathan/exemplogradleweb/ExemplogradlewebApplicationTests.java
@@ -0,0 +1,13 @@
+package karlinski.jonathan.exemplogradleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemplogradlewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
